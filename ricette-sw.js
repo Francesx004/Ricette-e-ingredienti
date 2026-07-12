ec804517aaef6a6b608c3e86967b1ac7823c8c19
Francesx004 <niofra781@gmail.com>
2026-07-12 10:09:50 +0200

    Add files via upload

---
+++ ricette-sw.js
@@ -0,0 +1,29 @@
+// Service worker: conserva una copia dell'app sul telefono per l'uso offline
+const CACHE = 'ricette-v2';
+const FILES = ['./ricette.html'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request, { ignoreSearch: true }))
+  );
+});
